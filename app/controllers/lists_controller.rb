19854db3753887a19da583d247f2b36ebde28a33
Jack Reuter <djreuter45@gmail.com>
2018-02-02 11:48:54 -0700

    started adding methods to controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,30 @@
 class ListsController < ApplicationController
-  
+  before_action :list_params
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = lists.build
+  end
+
+  def create
+    @list = lists.build(list_params)
+
+    if @list.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
