2066dc21722163796ae08cd569f5d11288c90be6
Jack Reuter <djreuter45@gmail.com>
2018-02-02 13:01:15 -0700

    added create and delete methods to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  def create
+    @list = List.find(params[:id])
+    @task = @list.tasks.create(task_params)
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @task = @list.tasks.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task)
+  end
 end
