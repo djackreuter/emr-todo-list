959035e9e7dbe54a537d97cc65069cede1f48bcd
Jack Reuter <djreuter45@gmail.com>
2018-02-02 13:17:45 -0700

    added nested attributes to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
-  has_many :tasks, inverse_of: :list
+  has_many :tasks, inverse_of: :list, dependent: :destroy
   validates :name, presence: true
+  accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: proc { |attr| attr['name'.blank?] }
 end
