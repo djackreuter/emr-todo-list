fda9ac79d5b268cfaa4364b60edfdf8aadbefff2
Jack Reuter <djreuter45@gmail.com>
2018-02-05 10:44:24 -0700

    documented helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
   def link_to_add_new_task(name, f, association, **args)
+    # create a new object that takes the form builder.  Get the object of
+    # that form builder and send the association
     new_object = f.object.send(association).klass.new
+    # get unique obj id so every new form task will have its own unique id
     id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |ff| render(association.to_s.singularize, f: ff)
+    fields = f.fields_for(association, new_object, child_index: id) do |ff|
+      render(association.to_s.singularize, f: ff)
     end
-    link_to(name, '#', class: 'add_fields ' + args[:class], data: {id: id, fields: fields.gsub("\n", "") })
+    link_to(name, '#', class: 'add_fields ' + args[:class], data: { id: id, fields: fields.gsub("\n", "") })
   end
 end
