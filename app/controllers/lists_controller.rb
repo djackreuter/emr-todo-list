ee569ef639ea512c3a6ddf97c4398bcbae55b0b3
Jack Reuter <djreuter45@gmail.com>
2018-02-06 09:26:37 -0700

    added edit and update methods

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,6 +13,10 @@ class ListsController < ApplicationController
     @list.tasks.build
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
   def create
     list = List.new(list_params)
 
@@ -20,6 +24,15 @@ class ListsController < ApplicationController
       redirect_to root_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to list_path(@list)
+    else
+      render 'edit'
     end
   end
 
@@ -32,6 +45,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, tasks_attributes: [:task, :_destroy])
+    params.require(:list).permit(:name, tasks_attributes: [:id, :task, :_destroy])
   end
 end
