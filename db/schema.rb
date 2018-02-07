3e82648bfd6c7ce410430e73d183411eab649120
Jack Reuter <djreuter45@gmail.com>
2018-02-07 11:42:48 -0700

    ran migration for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202195614) do
+ActiveRecord::Schema.define(version: 20180207181003) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,20 @@ ActiveRecord::Schema.define(version: 20180202195614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "list_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "bio"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "university"
+    t.string "degree"
+    t.integer "grad_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
