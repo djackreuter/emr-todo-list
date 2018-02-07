2420067ec372ccc879e8d1917d326d6bd348b7ee
Jack Reuter <djreuter45@gmail.com>
2018-02-07 16:11:59 -0700

    updated controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,14 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    set_partial
   end
 
   def create
     @user = User.new(user_params)
+    set_partial
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to edit_user_path(@user), method: :get
     else
       render 'new'
     end
@@ -22,14 +24,17 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    set_partial
+    render 'new'
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to user_path(@user)
+    set_partial
+    if @user.update_attributes(user_params)
+      redirect_to edit_user_path(@user)
     else
-      render 'edit'
+      render 'new'
     end
   end
 
@@ -43,5 +48,15 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :bio, :city, :state, :zip, :university, :degree, :grad_year)
+  end
+
+  def set_partial
+    if helpers.form1_empty?(@user)
+      @partial = 'form1'
+    elsif helpers.form2_empty?(@user)
+      @partial = 'form2'
+    else
+      @partial = 'form3'
+    end
   end
 end
