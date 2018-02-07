a52152cd124c2bf1e7fe0debde35e939598454de
Jack Reuter <djreuter45@gmail.com>
2018-02-07 16:12:16 -0700

    fixed model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,24 @@ class User < ApplicationRecord
   validates :bio, presence: true
 
   # section 2
-  with_options if: :sec1_present? do |sec2|
+  with_options if: :sec2_present? do |sec2|
     sec2.validates :city, presence: true
     sec2.validates :state, presence: true
     sec2.validates :zip, presence: true, length: { maximum: 5 }
   end
 
   # section 3
-  with_options if: :sec2_present? do |sec3|
-    sec3.validates :university, presence: true, on: :update
-    sec3.validates :degree, presence: true, on: :update
-    sec3.validates :grad_year, presence: true, on: :update, length: { maximum: 4 }
-  end
-
-  def sec1_present?
-    name.present? && email.present? && bio.present?
+  with_options if: :sec3_present? do |sec3|
+    sec3.validates :university, presence: true
+    sec3.validates :degree, presence: true
+    sec3.validates :grad_year, presence: true, length: { maximum: 4 }
   end
 
   def sec2_present?
+    !city.nil? || !state.nil? || !zip.nil?
+  end
+
+  def sec3_present?
     city.present? && state.present? && zip.present?
   end
 end
