3bcd38d8f5f3ae73dbf0b4fa93816b40c1e2d90e
Jack Reuter <djreuter45@gmail.com>
2018-02-13 10:24:33 -0700

    added create and destroy methods to document controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,4 +6,26 @@ class DocumentsController < ApplicationController
   def show
     @doc = Document.find(params[:id])
   end
+
+  def create
+    @doc = Document.new(doc_params)
+
+    if @doc.save
+      redirect_to document_path(@doc)
+    else
+      render 'index'
+    end
+  end
+
+  def destroy
+    @doc = Document.find(params[:id])
+    @doc.destroy
+    redirect_to documents_path
+  end
+
+  private
+
+  def doc_params
+    params.require(:document).permit(:name, :doc_image)
+  end
 end
