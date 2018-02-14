90b7d110f0a4679008098f10b071399f972834ec
Jack Reuter <djreuter45@gmail.com>
2018-02-14 12:52:34 -0700

    added validation for document file

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ApplicationRecord
   validates :name, presence: true
-
+  validates :doc_file, presence: true
   has_attached_file :doc_file
   validates_attachment :doc_file, content_type: { content_type: "application/pdf" }
 end
